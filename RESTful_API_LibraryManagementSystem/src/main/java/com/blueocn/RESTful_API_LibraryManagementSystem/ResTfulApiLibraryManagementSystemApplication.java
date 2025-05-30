f341a41f4648faf1480fbcab3c9225e9c3bc7709
BlueOCN <robgonzalezmu@gmail.com>
2025-05-30 09:38:29 -0600

    Update - Spring Boot project imported from Spring Initializer

---
+++ RESTful_API_LibraryManagementSystem/src/main/java/com/blueocn/RESTful_API_LibraryManagementSystem/ResTfulApiLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.blueocn.RESTful_API_LibraryManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiLibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ RESTful_API_LibraryManagementSystem/src/test/java/com/blueocn/RESTful_API_LibraryManagementSystem/ResTfulApiLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueocn.RESTful_API_LibraryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
